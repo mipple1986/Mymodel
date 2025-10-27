0768915715ae6402a03d2d1a687da5be356a7e27
mipple1986 <141001@xxmu.edu.cn>
2025-10-27 08:32:57 +0800

    Initial commit

---
+++ train_BCE.py
@@ -0,0 +1,302 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  1 16:13:59 2024
+
+@author: Administrator
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 29 09:58:03 2024
+
+@author: Administrator
+"""
+import os
+os.getcwd()
+os.chdir("C:/Users/Administrator/Desktop/keras-dilated_unet-master/")
+
+import numpy as np
+from glob import glob
+import tensorflow as tf
+import tensorflow.keras.backend as K
+from tensorflow import keras
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, Dense, Conv2D, concatenate
+import swin_layers
+import transformer_layers
+import utils
+import random
+import matplotlib.pyplot as plt
+import cv2
+import imageio
+from patchify import patchify, unpatchify
+from sklearn.metrics import confusion_matrix, roc_curve, auc, roc_auc_score, accuracy_score
+
+filter_num_begin = 128     # number of channels in the first downsampling block; it is also the number of embedded dimensions
+depth = 4                  # the depth of SwinUNET; depth=4 means three down/upsampling levels and a bottom level 
+stack_num_down = 2         # number of Swin Transformers per downsampling level
+stack_num_up = 2           # number of Swin Transformers per upsampling level
+patch_size = (2, 2)        # Extract 4-by-4 patches from the input image. Height and width of the patch must be equal.
+num_heads = [4, 8, 16, 16]   # number of attention heads per down/upsampling level
+window_size = [2, 2, 2, 2] # the size of attention window per down/upsampling level
+num_mlp = 512              # number of MLP nodes within the Transformer
+shift_window=True          # Apply window shifting, i.e., Swin-MSA
+
+input_size = (96, 96, 1)
+IN = Input(input_size)
+
+def swin_transformer_stack(X, stack_num, embed_dim, num_patch, num_heads, window_size, num_mlp, shift_window=True, name=''):
+    '''
+    Stacked Swin Transformers that share the same token size.
+    
+    Alternated Window-MSA and Swin-MSA will be configured if `shift_window=True`, Window-MSA only otherwise.
+    *Dropout is turned off.
+    '''
+    # Turn-off dropouts
+    mlp_drop_rate = 0 # Droupout after each MLP layer
+    attn_drop_rate = 0 # Dropout after Swin-Attention
+    proj_drop_rate = 0 # Dropout at the end of each Swin-Attention block, i.e., after linear projections
+    drop_path_rate = 0 # Drop-path within skip-connections
+    
+    qkv_bias = True # Convert embedded patches to query, key, and values with a learnable additive value
+    qk_scale = None # None: Re-scale query based on embed dimensions per attention head # Float for user specified scaling factor
+    
+    if shift_window:
+        shift_size = window_size // 2
+    else:
+        shift_size = 0
+    
+    for i in range(stack_num):
+    
+        if i % 2 == 0:
+            shift_size_temp = 0
+        else:
+            shift_size_temp = shift_size
+
+        X = swin_layers.SwinTransformerBlock(dim=embed_dim, 
+                                             num_patch=num_patch, 
+                                             num_heads=num_heads, 
+                                             window_size=window_size, 
+                                             shift_size=shift_size_temp, 
+                                             num_mlp=num_mlp, 
+                                             qkv_bias=qkv_bias, 
+                                             qk_scale=qk_scale,
+                                             mlp_drop=mlp_drop_rate, 
+                                             attn_drop=attn_drop_rate, 
+                                             proj_drop=proj_drop_rate, 
+                                             drop_path_prob=drop_path_rate, 
+                                             name='name{}'.format(i))(X)
+    return X
+
+
+def swin_unet_2d_base(input_tensor, filter_num_begin, depth, stack_num_down, stack_num_up, 
+                      patch_size, num_heads, window_size, num_mlp, shift_window=True, name='swin_unet'):
+    '''
+    The base of Swin-UNET.
+    
+    The general structure:
+    
+    1. Input image --> a sequence of patches --> tokenize these patches
+    2. Downsampling: swin-transformer --> patch merging (pooling)
+    3. Upsampling: concatenate --> swin-transfprmer --> patch expanding (unpooling)
+    4. Model head
+    
+    '''
+    # Compute number be patches to be embeded
+    input_size = input_tensor.shape.as_list()[1:]
+    num_patch_x = input_size[0]//patch_size[0]
+    num_patch_y = input_size[1]//patch_size[1]
+    
+    # Number of Embedded dimensions
+    embed_dim = filter_num_begin
+    
+    depth_ = depth
+    
+    X_skip = []
+
+    X = input_tensor
+    
+    # Patch extraction
+    X = transformer_layers.patch_extract(patch_size)(X)
+
+    # Embed patches to tokens
+    X = transformer_layers.patch_embedding(num_patch_x*num_patch_y, embed_dim)(X)
+    
+    # The first Swin Transformer stack
+    X = swin_transformer_stack(X, 
+                               stack_num=stack_num_down, 
+                               embed_dim=embed_dim, 
+                               num_patch=(num_patch_x, num_patch_y), 
+                               num_heads=num_heads[0], 
+                               window_size=window_size[0], 
+                               num_mlp=num_mlp, 
+                               shift_window=shift_window, 
+                               name='{}_swin_down0'.format(name))
+    X_skip.append(X)
+    
+    # Downsampling blocks
+    for i in range(depth_-1):
+        
+        # Patch merging
+        X = transformer_layers.patch_merging((num_patch_x, num_patch_y), embed_dim=embed_dim, name='down{}'.format(i))(X)
+        
+        # update token shape info
+        embed_dim = embed_dim*2
+        num_patch_x = num_patch_x//2
+        num_patch_y = num_patch_y//2
+        
+        # Swin Transformer stacks
+        X = swin_transformer_stack(X, 
+                                   stack_num=stack_num_down, 
+                                   embed_dim=embed_dim, 
+                                   num_patch=(num_patch_x, num_patch_y), 
+                                   num_heads=num_heads[i+1], 
+                                   window_size=window_size[i+1], 
+                                   num_mlp=num_mlp, 
+                                   shift_window=shift_window, 
+                                   name='{}_swin_down{}'.format(name, i+1))
+        
+        # Store tensors for concat
+        X_skip.append(X)
+        
+    # reverse indexing encoded tensors and hyperparams
+    X_skip = X_skip[::-1]
+    num_heads = num_heads[::-1]
+    window_size = window_size[::-1]
+    
+    # upsampling begins at the deepest available tensor
+    X = X_skip[0]
+    
+    # other tensors are preserved for concatenation
+    X_decode = X_skip[1:]
+    
+    depth_decode = len(X_decode)
+    
+    for i in range(depth_decode):
+        
+        # Patch expanding
+        X = transformer_layers.patch_expanding(num_patch=(num_patch_x, num_patch_y), 
+                                               embed_dim=embed_dim, 
+                                               upsample_rate=2, 
+                                               return_vector=True)(X)
+        
+
+        # update token shape info
+        embed_dim = embed_dim//2
+        num_patch_x = num_patch_x*2
+        num_patch_y = num_patch_y*2
+        
+        # Concatenation and linear projection
+        X = concatenate([X, X_decode[i]], axis=-1, name='{}_concat_{}'.format(name, i))
+        X = Dense(embed_dim, use_bias=False, name='{}_concat_linear_proj_{}'.format(name, i))(X)
+        
+        # Swin Transformer stacks
+        X = swin_transformer_stack(X, 
+                                   stack_num=stack_num_up, 
+                                   embed_dim=embed_dim, 
+                                   num_patch=(num_patch_x, num_patch_y), 
+                                   num_heads=num_heads[i], 
+                                   window_size=window_size[i], 
+                                   num_mlp=num_mlp, 
+                                   shift_window=shift_window, 
+                                   name='{}_swin_up{}'.format(name, i))
+        
+    # The last expanding layer; it produces full-size feature maps based on the patch size
+    # !!! <--- "patch_size[0]" is used; it assumes patch_size = (size, size)
+    
+    X = transformer_layers.patch_expanding(num_patch=(num_patch_x, num_patch_y), 
+                                           embed_dim=embed_dim, 
+                                           upsample_rate=patch_size[0], 
+                                           return_vector=False)(X)
+    
+    return X
+
+###################### main
+
+
+import configparser
+import tensorflow as tf
+from tensorflow.keras.models import Model
+from tensorflow.python.keras.layers import Input, concatenate, Conv2D, MaxPooling2D, UpSampling2D, Reshape, core, Dropout
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import ModelCheckpoint, LearningRateScheduler
+from tensorflow.keras import backend as K
+from tensorflow.keras.utils import plot_model as plot
+from tensorflow.keras.optimizers import SGD
+from tensorflow.keras import optimizers
+#function to obtain data for training/testing (validation)
+from lib.extract_patches import get_data_training
+#import lovaze_softmax as ls
+from tensorflow.keras.callbacks import LearningRateScheduler
+import mylr as mlr
+from tensorflow.keras.layers import BatchNormalization,PReLU,LeakyReLU,Conv2DTranspose
+
+
+
+import sys
+#sys.path.insert(0, './lib/')
+from help_functions import *
+
+config = configparser.RawConfigParser()
+config.read('configuration.txt')
+#patch to the datasets
+path_data = config.get('data paths', 'path_local')
+#Experiment name
+name_experiment = config.get('experiment name', 'name')
+#training settings
+N_epochs = int(config.get('training settings', 'N_epochs'))
+batch_size = int(config.get('training settings', 'batch_size'))
+
+
+
+#============ Load the data and divided in patches
+patches_imgs_train, patches_masks_train = get_data_training(
+    DRIVE_train_imgs_original = path_data + config.get('data paths', 'train_imgs_original'),
+    DRIVE_train_groudTruth = path_data + config.get('data paths', 'train_groundTruth'),  #masks
+    patch_height = int(config.get('data attributes', 'patch_height')),
+    patch_width = int(config.get('data attributes', 'patch_width')),
+    N_subimgs = int(config.get('training settings', 'N_subimgs')),
+    inside_FOV = config.getboolean('training settings', 'inside_FOV') #select the patches only inside the FOV  (default == True)
+)
+
+#========= Save a sample of what you're feeding to the neural network ==========
+N_sample = min(patches_imgs_train.shape[0],40)
+visualize(group_images(patches_imgs_train[0:N_sample,:,:,:],5),'./'+name_experiment+'/'+"sample_input_imgs")#.show()
+visualize(group_images(patches_masks_train[0:N_sample,:,:,:],5),'./'+name_experiment+'/'+"sample_input_masks")#.show()
+
+
+#=========== Construct and save the model arcitecture =====
+print(patches_imgs_train.shape)
+n_ch = patches_imgs_train.shape[1]
+patch_height = patches_imgs_train.shape[2]
+patch_width = patches_imgs_train.shape[3]
+patches_imgs_train = np.transpose(patches_imgs_train,(0,2,3,1))
+patches_masks_train = np.transpose(patches_masks_train,(0,2,3,1))
+#patches_masks_train = masks_Unet(patches_masks_train)  # reduce memory consumption
+
+os.environ["CUDA_VISIBLE_DEVICES"] = '0'
+
+X = swin_unet_2d_base(IN, filter_num_begin, depth, stack_num_down, stack_num_up,
+                      patch_size, num_heads, window_size, num_mlp,
+                      shift_window=shift_window, name='swin_unet')
+
+# Output section
+n_labels = 1
+OUT = Conv2D(n_labels, kernel_size=1, use_bias=True, activation='sigmoid')(X)
+# Model configuration
+model = Model(inputs=[IN, ], outputs=[OUT, ])
+
+opt = keras.optimizers.Adam(learning_rate=1e-4, clipvalue=0.5)
+# opt = keras.optimizers.SGD(lr=0.001, decay=1e-6, momentum=0.9, nesterov=True)
+
+model.compile(loss=keras.losses.binary_crossentropy, optimizer=opt)
+
+# model.load_weights('./swin_unet_BCE_augm4500.h5')
+
+batch_size = 5
+model.fit(patches_imgs_train[0:100], patches_masks_train[0:100], epochs=N_epochs, batch_size=batch_size, verbose=1, shuffle=True, validation_split=0.1)
+
+
+model.summary()
+patches_imgs_train.shape
+patches_masks_train.shape
